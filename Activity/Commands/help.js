728dc62e985d685aaf0cd7f6b2980490323bf39b
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-05-27 20:17:33 +0530

    Help Command is implemented

---
+++ Activity/Commands/help.js
@@ -0,0 +1,8 @@
+module.exports.help = function(){
+    console.log(`Help is a utility to view all available commands list. Please use these commands to see various utilities available:
+    1. View as Tree => node manas view src -t
+    2. View as List => node manas view src -f
+    3. Untreefy the directory => node manas untreefy src dest
+    4. Treefy the director => node manas treefy src dest
+    `);
+};
