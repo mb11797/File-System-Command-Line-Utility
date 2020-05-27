897bee805f59a22ee77d94f12acf1760c5d4c348
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-05-27 20:10:42 +0530

    Respective Commands Mapping file implemented

---
+++ Activity/manas.js
@@ -0,0 +1,24 @@
+let {view} = require('./Commands/view');
+let {treefy} = require('./Commands/treefy');
+let {untreefy} = require('./Commands/untreefy');
+let {help} = require('./Commands/help');
+
+let cmd = process.argv[2];
+
+switch(cmd){
+    case "view":
+        view(process.argv[3], args[4]);
+        break;
+    case "treefy":
+        treefy(process.argv[3], process.argv[4]);
+        break;
+    case "untreefy":
+        untreefy(process.argv[3], process.argv[4]);
+        break;
+    case "help":
+        help();
+        break;
+    default:
+        console.log("Wrong command");
+        break;
+}
